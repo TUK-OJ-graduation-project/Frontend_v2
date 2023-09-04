0573f9d6af63fb4d622faf0dd026261b788f8f0d
goYoungOd <lyslys77@tukorea.ac.kr>
2023-09-05 03:08:45 +0900

    .

--- src/component/ChoicesProblems/MCQDetail.jsx
+++ src/component/ChoicesProblems/MCQDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
-import './MCQDetail.css'; 
+import './MCQDetail.css';
 
 const MCQDetail = () => {
     const [mcq, setMcq] = useState(null);
